c55482d961aa2ac394151afcbd1c578e18252874
Bingil35 <165053660+Bingil35@users.noreply.github.com>
2024-10-16 22:20:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Model CNN biospy!!!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This my first app. The application is model CNN biospy."
 )
